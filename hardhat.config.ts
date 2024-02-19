96c57d0cd2ef8d4a4a31782384bfcc37dd5ad462
Michalis Kargakis <kargakis@protonmail.com>
2024-02-19 19:11:07 +0100

    Extend Hardhat config with optimizer runs and new compiler version

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,26 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+const sharedCompilerConfig = {
+  optimizer: {
+    enabled: true,
+    runs: 200,
+  }
+};
+
 const config: HardhatUserConfig = {
-  solidity: "0.8.14",
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.14',
+        settings: sharedCompilerConfig,
+      },
+      {
+        version: '0.8.19', // needed by prb-math
+        settings: sharedCompilerConfig,
+      },
+    ],
+  },
 };
 
 export default config;
